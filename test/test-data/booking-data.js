1b4cea624519b8ce457959e28343637a9cec361a
danielsolomon332 <daniel.solomon332@gmail.com>
2022-01-14 16:42:24 -0700

    Add all test data

---
+++ test/test-data/booking-data.js
@@ -0,0 +1,74 @@
+const bookings = [
+  {
+    "id": "5fwrgu4i7k55hl8dj",
+    "userID": 14,
+    "date": "2022/02/07",
+    "roomNumber": 1,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6tf",
+    "userID": 36,
+    "date": "2022/01/25",
+    "roomNumber": 2,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6vb",
+    "userID": 22,
+    "date": "2022/01/21",
+    "roomNumber": 3,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6vd",
+    "userID": 24,
+    "date": "2022/01/23",
+    "roomNumber": 4,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6vq",
+    "userID": 46,
+    "date": "2022/02/24",
+    "roomNumber": 5,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6x4",
+    "userID": 9,
+    "date": "2022/01/27",
+    "roomNumber": 6,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6xt",
+    "userID": 9,
+    "date": "2022/01/27",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6ye",
+    "userID": 4,
+    "date": "2022/02/05",
+    "roomNumber": 8,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6z9",
+    "userID": 24,
+    "date": "2022/02/04",
+    "roomNumber": 9,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6zw",
+    "userID": 14,
+    "date": "2022/01/15",
+    "roomNumber": 10,
+    "roomServiceCharges": []
+  }
+]
+
+export default bookings;
