3c2d3633d2e1dd6a6764a06c6d42675b31f7efca
danielsolomon332 <daniel.solomon332@gmail.com>
2022-01-14 19:56:41 -0700

    Create booking class

--- test/booking-test.js
+++ test/booking-test.js
@@ -1,4 +1,4 @@
-import { expect } for 'chai';
+import { expect } from 'chai';
 import bookingData from './test-data/booking-data';
 import Booking from '../src/classes/booking';
 
@@ -7,30 +7,29 @@ describe('Booking', () => {
 
   beforeEach(() => {
     booking = new Booking(bookingData[0]);
-  })
+  });
 
   it('Should be a function', () => {
     expect(Booking).to.be.a('function');
-  })
+  });
 
   it('Should have a booking id', () => {
     expect(booking.id).to.equal(bookingData[0].id)
-  })
+  });
 
   it('Should have a userID', () => {
     expect(booking.userID).to.equal(bookingData[0].userID)
-  })
+  });
 
-  it('Should have a booking data', () => {
-    expect(booking.data).to.equal(bookingData[0].data)
-  })
+  it('Should have a booking date', () => {
+    expect(booking.date).to.equal(bookingData[0].date)
+  });
 
   it('Should have a room number', () => {
     expect(booking.roomNumber).to.equal(bookingData[0].roomNumber)
-  })
+  });
 
   it('Should have room service charges', () => {
     expect(booking.roomServiceCharges).to.equal(bookingData[0].roomServiceCharges)
-  })
-
-})
+  });
+});
