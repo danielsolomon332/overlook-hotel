066424958ec1cdaec39c7c70f1350ec78d5f8a16
danielsolomon332 <daniel.solomon332@gmail.com>
2022-01-17 11:54:43 -0700

    Setup api calls

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,26 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
 var dateControl = document.querySelector('input[type="date"]');
 dateControl.value;
 console.log(dateControl.value);
 console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
+
+import {getBookingsData, getCustomersData, getRoomsData} from './apiCalls'
+
+
+fetchAllData() {
+  return Promise.all([getBookingsData(), getCustomersData(), getRoomsData()])
+  .then(data => allData(data))
+  .catch(error => displayError(error))
+}
+
+// function allData(data) {
+//
+// }
+//
+// function displayError(error) {
+//
+// }
